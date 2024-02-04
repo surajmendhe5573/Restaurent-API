aa3295b1045044e3a62e346d943123f37fb5be6c
surajmendhe5573 <surajmendhe470@gmail.com>
2024-02-04 15:39:12 +0530

    first commit

---
+++ models/MenuItem.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const menuItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    taste: {
+        type: String,
+        enum: ['sweet', 'spicy', 'sour'],
+        required: true,
+    },
+    num_sales: {
+        type: Number,
+        default: 0,
+    }
+})
+
+const MenuItem = mongoose.model('MenuItem', menuItemSchema);
+
+module.exports = MenuItem;
