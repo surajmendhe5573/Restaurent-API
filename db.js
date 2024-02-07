2fc0fdab965e50f574826f3b46d7bc8e9d074ec6
surajmendhe5573 <surajmendhe470@gmail.com>
2024-02-07 09:01:48 +0530

    Your commit message here

--- db.js
+++ db.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 // Define the MongoDB connection URL
 // const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
 const mongoURL = 'mongodb://localhost:27017/Restaurent'
-
 // Set up MongoDB connection
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
